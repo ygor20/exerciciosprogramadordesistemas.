eae0c3b629689e1e8a0cc0e5271a723dec16925c
ygor20 <ygor.aoliveira@senacsp.edu.br>
2025-03-28 16:16:53 -0300

    Menu

---
+++ atividademenu/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace atividademenu
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenu));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usuárioToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.catálogoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.camisetasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bermudasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.calçasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.camisasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreNósToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.femininasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.masculinasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infantilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.carrinhoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelFundo = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(1)))), ((int)(((byte)(25)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.homeToolStripMenuItem,
+            this.loginToolStripMenuItem,
+            this.catálogoToolStripMenuItem,
+            this.carrinhoToolStripMenuItem,
+            this.sobreNósToolStripMenuItem});
+            this.menuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(3, 2, 0, 0);
+            this.menuStrip1.Size = new System.Drawing.Size(778, 114);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // homeToolStripMenuItem
+            // 
+            this.homeToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(1)))), ((int)(((byte)(25)))));
+            this.homeToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.homeToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(84, 112);
+            this.homeToolStripMenuItem.Text = "Home";
+            // 
+            // loginToolStripMenuItem
+            // 
+            this.loginToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(1)))), ((int)(((byte)(25)))));
+            this.loginToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usuárioToolStripMenuItem1});
+            this.loginToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.loginToolStripMenuItem.Name = "loginToolStripMenuItem";
+            this.loginToolStripMenuItem.Overflow = System.Windows.Forms.ToolStripItemOverflow.AsNeeded;
+            this.loginToolStripMenuItem.Size = new System.Drawing.Size(81, 112);
+            this.loginToolStripMenuItem.Text = "Login";
+            // 
+            // usuárioToolStripMenuItem1
+            // 
+            this.usuárioToolStripMenuItem1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(1)))), ((int)(((byte)(25)))));
+            this.usuárioToolStripMenuItem1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usuárioToolStripMenuItem1.ForeColor = System.Drawing.Color.White;
+            this.usuárioToolStripMenuItem1.Name = "usuárioToolStripMenuItem1";
+            this.usuárioToolStripMenuItem1.Size = new System.Drawing.Size(180, 26);
+            this.usuárioToolStripMenuItem1.Text = "Usuário";
+            // 
+            // catálogoToolStripMenuItem
+            // 
+            this.catálogoToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(1)))), ((int)(((byte)(25)))));
+            this.catálogoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.camisetasToolStripMenuItem,
+            this.bermudasToolStripMenuItem,
+            this.calçasToolStripMenuItem,
+            this.camisasToolStripMenuItem});
+            this.catálogoToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.catálogoToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.catálogoToolStripMenuItem.Name = "catálogoToolStripMenuItem";
+            this.catálogoToolStripMenuItem.Size = new System.Drawing.Size(113, 112);
+            this.catálogoToolStripMenuItem.Text = "Catálogo";
+            // 
+            // camisetasToolStripMenuItem
+            // 
+            this.camisetasToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(1)))), ((int)(((byte)(25)))));
+            this.camisetasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.femininasToolStripMenuItem,
+            this.masculinasToolStripMenuItem,
+            this.infantilToolStripMenuItem});
+            this.camisetasToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.camisetasToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.camisetasToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("camisetasToolStripMenuItem.Image")));
+            this.camisetasToolStripMenuItem.Name = "camisetasToolStripMenuItem";
+            this.camisetasToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.camisetasToolStripMenuItem.Text = "Camisetas";
+            // 
+            // bermudasToolStripMenuItem
+            // 
+            this.bermudasToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(1)))), ((int)(((byte)(25)))));
+            this.bermudasToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bermudasToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.bermudasToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("bermudasToolStripMenuItem.Image")));
+            this.bermudasToolStripMenuItem.Name = "bermudasToolStripMenuItem";
+            this.bermudasToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.bermudasToolStripMenuItem.Text = "Bermudas";
+            // 
+            // calçasToolStripMenuItem
+            // 
+            this.calçasToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(1)))), ((int)(((byte)(25)))));
+            this.calçasToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calçasToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.calçasToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("calçasToolStripMenuItem.Image")));
+            this.calçasToolStripMenuItem.Name = "calçasToolStripMenuItem";
+            this.calçasToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.calçasToolStripMenuItem.Text = "Calças";
+            // 
+            // camisasToolStripMenuItem
+            // 
+            this.camisasToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(1)))), ((int)(((byte)(25)))));
+            this.camisasToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.camisasToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.camisasToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("camisasToolStripMenuItem.Image")));
+            this.camisasToolStripMenuItem.Name = "camisasToolStripMenuItem";
+            this.camisasToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.camisasToolStripMenuItem.Text = "Camisas";
+            // 
+            // sobreNósToolStripMenuItem
+            // 
+            this.sobreNósToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(1)))), ((int)(((byte)(25)))));
+            this.sobreNósToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sobreNósToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.sobreNósToolStripMenuItem.Name = "sobreNósToolStripMenuItem";
+            this.sobreNósToolStripMenuItem.Size = new System.Drawing.Size(121, 112);
+            this.sobreNósToolStripMenuItem.Text = "Sobre Nós";
+            this.sobreNósToolStripMenuItem.Click += new System.EventHandler(this.sobreNósToolStripMenuItem_Click);
+            // 
+            // femininasToolStripMenuItem
+            // 
+            this.femininasToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(1)))), ((int)(((byte)(25)))));
+            this.femininasToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.femininasToolStripMenuItem.Name = "femininasToolStripMenuItem";
+            this.femininasToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.femininasToolStripMenuItem.Text = "Femininas";
+            // 
+            // masculinasToolStripMenuItem
+            // 
+            this.masculinasToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(1)))), ((int)(((byte)(25)))));
+            this.masculinasToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.masculinasToolStripMenuItem.Name = "masculinasToolStripMenuItem";
+            this.masculinasToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.masculinasToolStripMenuItem.Text = "Masculinas";
+            // 
+            // infantilToolStripMenuItem
+            // 
+            this.infantilToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(1)))), ((int)(((byte)(25)))));
+            this.infantilToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.infantilToolStripMenuItem.Name = "infantilToolStripMenuItem";
+            this.infantilToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.infantilToolStripMenuItem.Text = "Infantil";
+            // 
+            // carrinhoToolStripMenuItem
+            // 
+            this.carrinhoToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(1)))), ((int)(((byte)(25)))));
+            this.carrinhoToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.carrinhoToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.carrinhoToolStripMenuItem.Name = "carrinhoToolStripMenuItem";
+            this.carrinhoToolStripMenuItem.Size = new System.Drawing.Size(105, 112);
+            this.carrinhoToolStripMenuItem.Text = "Carrinho";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(1)))), ((int)(((byte)(25)))));
+            this.toolStripMenuItem1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripMenuItem1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripMenuItem1.Image")));
+            this.toolStripMenuItem1.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Overflow = System.Windows.Forms.ToolStripItemOverflow.AsNeeded;
+            this.toolStripMenuItem1.Padding = new System.Windows.Forms.Padding(4, 0, 4, 5);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(130, 112);
+            this.toolStripMenuItem1.Text = " ";
+            // 
+            // panelFundo
+            // 
+            this.panelFundo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(1)))), ((int)(((byte)(25)))));
+            this.panelFundo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelFundo.Location = new System.Drawing.Point(140, 106);
+            this.panelFundo.Name = "panelFundo";
+            this.panelFundo.Size = new System.Drawing.Size(498, 384);
+            this.panelFundo.TabIndex = 1;
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(778, 489);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.panelFundo);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMenu";
+            this.Text = "Loja SP ";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loginToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem catálogoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreNósToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem camisetasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usuárioToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem bermudasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem calçasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem camisasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem femininasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem masculinasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infantilToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem carrinhoToolStripMenuItem;
+        private System.Windows.Forms.Panel panelFundo;
+    }
+}
+
---
+++ atividademenu/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace atividademenu
+{
+    public partial class frmMenu : Form
+    {
+        public frmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void sobreNósToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("A Loja SP foi fundada em 2025 com a intenção de produzir peças de qualidade a um preço acessivel.", "Sobre Nós" );
+        }
+    }
+}
